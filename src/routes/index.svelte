ebbfd4c375cd3e365bbf505d653df43cab292ae1
babakfp <44144724+babakfp@users.noreply.github.com>
2022-02-28 18:00:44 -0800

    Added title and meta seo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,4 +2,9 @@
 	import README from '$lib/README.md'
 </script>
 
+<svelte:head>
+	<title>I Like Svelte But, I Hate It! - What nobody tells you!</title>
+	<meta name="description" content="I'm using Svelte sense 2020 and this is why I really hate it." />
+</svelte:head>
+
 <README />
