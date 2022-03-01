7b1920eda694abdb393099a97b08f29e1d28ce6b
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-01 09:47:13 -0800

    Added make svelte better stuff

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,19 @@
   import '../app.css'
 </script>
 
+<header>
+	<nav>
+		<ul class="flex flex-wrap justify-center">
+			<li>
+				<a class="block py-3 px-4" href="/">I Like Svelte But, I Hate It!</a>
+			</li>
+			<li>
+				<a class="block py-3 px-4" href="/make-svelte-better">Make Svelte Better</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
 <main class="container content py-8 sm:py-20">
 	<div class="max-w-3xl mx-auto">
 		<slot />
---
+++ src/routes/make-svelte-better.svelte
@@ -0,0 +1,40 @@
+<script>
+	import ConditionalBlocks from '$lib/make-svelte-better/01. Conditional Blocks .md'
+	import EachBlocks from '$lib/make-svelte-better/02. Each Blocks .md'
+	import AwaitBlock from '$lib/make-svelte-better/03. Await Block .md'
+	import DynamicTag from '$lib/make-svelte-better/04. Dynamic Tag .md'
+	import ClassProp from '$lib/make-svelte-better/05. Class Prop .md'
+	import Translation from '$lib/make-svelte-better/06. Translation .md'
+	import ComponentDispatch from '$lib/make-svelte-better/07. Component Dispatch .md'
+	import CommentsSyntax from '$lib/make-svelte-better/08. Comments Syntax .md'
+	import CSSScope from '$lib/make-svelte-better/09. CSS Scope .md'
+	import A11Y from '$lib/make-svelte-better/10. A11Y .md'
+	import Docs from '$lib/make-svelte-better/11. Docs .md'
+	import ParentChilds from '$lib/make-svelte-better/12. Parent Childs .md'
+	import ConnectChildToParent from '$lib/make-svelte-better/13. Connect Child To Parent .md'
+	import Tick from '$lib/make-svelte-better/13. Tick .md'
+	import DynamicClasses from '$lib/make-svelte-better/14. Dynamic Classes .md'
+</script>
+
+<svelte:head>
+	<title>Make Svelte Better</title>
+	<meta name="description" content="Some feature requests and enhancements that need to happen in Svelte." />
+</svelte:head>
+
+<h1>Make Svelte Better</h1>
+
+<ConditionalBlocks />
+<EachBlocks />
+<AwaitBlock />
+<DynamicTag />
+<ClassProp />
+<Translation />
+<ComponentDispatch />
+<CommentsSyntax />
+<CSSScope />
+<A11Y />
+<Docs />
+<ParentChilds />
+<ConnectChildToParent />
+<Tick />
+<DynamicClasses />
