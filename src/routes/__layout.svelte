858006cfcca41dbcd8186b316e2be464abed2258
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-01 11:19:46 -0800

    Added navigation animation

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { fly } from 'svelte/transition'
+	import { navigating } from '$app/stores'
   import '../app.css'
 </script>
 
@@ -16,7 +18,9 @@
 </header>
 
 <main class="container content py-8 sm:py-20">
-	<div class="max-w-3xl mx-auto">
-		<slot />
-	</div>
+	{#key $navigating}
+		<div class="content max-w-3xl mx-auto" in:fly={{ y: 6, duration: 300, delay: 300 }} out:fly={{ y: 6, duration: 300 }}>
+			<slot />
+		</div>
+	{/key}
 </main>
