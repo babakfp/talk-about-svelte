36174841a9af366dcc5fae17437ece0195cb0fcb
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-02 10:12:10 -0800

    Added new components

---
+++ src/lib/AlsoRead.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let title
+	export let href
+</script>
+
+<p>
+	<span>Also read:</span>
+	<a class="py-3 px-4" {href}>{title}</a>
+</p>
+<hr>
+<br>
---
+++ src/lib/PostDate.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let modify
+	export let publish
+</script>
+
+<div class="text-xs flex flex-wrap gap-4 opacity-50">
+	<span>Modify date: <b>{modify}</b></span>
+	<span>Publish date: <b>{publish}</b></span>
+</div>
