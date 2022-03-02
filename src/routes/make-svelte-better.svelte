d3437d7ddecbff3c10414a549ba38c2ba6365f6c
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-01 18:58:39 -0800

    Rewrites

--- src/routes/make-svelte-better.svelte
+++ src/routes/make-svelte-better.svelte
@@ -1,15 +1,14 @@
 <script>
 	import CodeBlocksInHTML from '$lib/make-svelte-better/01. Code Blocks In HTML .md'
 	import DynamicHTMLTag from '$lib/make-svelte-better/04. Dynamic HTML Tag .md'
-	import PropNamedClass from '$lib/make-svelte-better/05. Prop Named Class .md'
+	import ClassProp from '$lib/make-svelte-better/05. Class Prop  .md'
 	import Translation from '$lib/make-svelte-better/06. Translation .md'
-	import ComponentDispatch from '$lib/make-svelte-better/07. Component Dispatch .md'
+	import CreateEventDispatcher from '$lib/make-svelte-better/07. Create Event Dispatcher .md'
 	import CommentsSyntax from '$lib/make-svelte-better/08. Comments Syntax .md'
 	import CSSScope from '$lib/make-svelte-better/09. CSS Scope .md'
 	import A11Y from '$lib/make-svelte-better/10. A11Y .md'
-	import Docs from '$lib/make-svelte-better/11. Docs .md'
+	import Documentations from '$lib/make-svelte-better/11. Documentations .md'
 	import ParentChilds from '$lib/make-svelte-better/12. Parent Childs .md'
-	import ConnectChildToParent from '$lib/make-svelte-better/13. Connect Child To Parent .md'
 	import Tick from '$lib/make-svelte-better/13. Tick .md'
 	import DynamicClasses from '$lib/make-svelte-better/14. Dynamic Classes .md'
 </script>
@@ -32,11 +31,11 @@
 <hr>
 <DynamicHTMLTag />
 <hr>
-<PropNamedClass />
+<ClassProp />
 <hr>
 <Translation />
 <hr>
-<ComponentDispatch />
+<CreateEventDispatcher />
 <hr>
 <CommentsSyntax />
 <hr>
@@ -44,11 +43,9 @@
 <hr>
 <A11Y />
 <hr>
-<Docs />
+<Documentations />
 <hr>
 <ParentChilds />
-<hr>
-<ConnectChildToParent />
 <hr>
 <Tick />
 <hr>
