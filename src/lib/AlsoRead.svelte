480b47c7a0545857b87a891eee7df5df1fcfea89
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-07 07:33:55 -0800

    Added isNew label to AlsoRead

--- src/lib/AlsoRead.svelte
+++ src/lib/AlsoRead.svelte
@@ -1,11 +1,15 @@
 <script>
 	export let title
 	export let href
+	export let isNew = false
 </script>
 
 <p>
 	<span>Also read:</span>
 	<a {href}>{title}</a>
+	{#if isNew}
+		<span class="text-2xs text-yellow-400">New</span>
+	{/if}
 </p>
 <hr>
 <br>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,7 +10,7 @@
 	<meta name="description" content="I'm using Svelte sense 2020 and this is why I really hate it." />
 </svelte:head>
 
-<AlsoRead title="Make Svelte Better" href="/make-svelte-better" />
+<AlsoRead title="Make Svelte Better" href="/make-svelte-better" isNew={true} />
 
 <PostDate modify="2022-03-04" publish="2022-02-28" />
 
