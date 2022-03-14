69c3afa6659e55c4a20b2770cd75c2f0b27e11d8
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-14 14:51:51 -0700

    Removed page transitioin because it doesn't work properly

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,13 +1,9 @@
 <script>
-	import { fly } from 'svelte/transition'
-	import { navigating } from '$app/stores'
   import '../app.css'
 </script>
 
 <main class="container content py-8 sm:py-20">
-	{#key $navigating}
-		<div class="content max-w-3xl mx-auto" in:fly={{ y: 6, duration: 300, delay: 300 }} out:fly={{ y: 6, duration: 300 }}>
-			<slot />
-		</div>
-	{/key}
+	<div class="content max-w-3xl mx-auto">
+		<slot />
+	</div>
 </main>
