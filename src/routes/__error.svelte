c7364cb0e975b6013e7010da1a9e234aab4407bc
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-24 08:42:39 -0700

    Added error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,53 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: { error, status }
+		}
+	}
+</script>
+
+<script>
+	import { dev } from '$app/env'
+
+	import { posts } from ':store/posts.js'
+	import { PostCard } from ':components/Post'
+
+	export let error
+	export let status
+</script>
+
+<svelte:head>
+	<title>{status}</title>
+</svelte:head>
+
+<div>
+	<h1 class="text-center font-bold text-7xl">{status}</h1>
+
+	{#if status === 404}
+
+		<p class="mt-4 text-center">The page you were looking for could not be found.</p>
+
+		<hr class="border-white opacity-20 my-12">
+
+		<!-- {#if !dev} -->
+			<div class="grid gap-4">
+				{#each $posts as post}
+					<PostCard {...post} />
+				{/each}
+			</div>
+		<!-- {/if} -->
+
+	{:else}
+
+		<h2 class="text-center">{error.message}</h2>
+
+		{#if error.stack && dev}
+			<code class="scrollbar scrollbar-x-spacing-lg block mt-8 p-4 bg-gray-200 rounded dir-ltr lg:p-8">
+				<pre>
+					{error.stack}
+				</pre>
+			</code>
+		{/if}
+
+	{/if}
+</div>
