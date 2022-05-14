7a2c57c04e70e1fee3215361a85569828a2c3fbe
babakfp <44144724+babakfp@users.noreply.github.com>
2022-05-14 05:26:28 -0700

    removed named layouts

--- src/store/posts.js
+++ src/store/posts.js
@@ -7,16 +7,6 @@ const defaultAlsoRead = {
 
 export const posts = readable([
   {
-		title: 'SvelteKit Named Layouts',
-		description: "Let's talk about it.",
-		href: '/blog/sveltekit-named-layouts',
-    date: {
-      modifiedAt: '2022-05-13',
-      publishedAt: '2022-05-13',
-    },
-    alsoRead: defaultAlsoRead,
-	},
-  {
 		title: 'Make Svelte Better',
 		description: 'Missing features and etc in Svelte...',
 		href: '/blog/make-svelte-better',
